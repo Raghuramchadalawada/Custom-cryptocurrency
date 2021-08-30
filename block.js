a8688d49560c9a60b1c45c47bd486335966cc1dd
Raghuramchadalawada <raghusrhyme@gmail.com>
2021-08-30 19:18:31 +0530

    Hash of the each block using SHA256 hashing function

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class block{
   constructor(timestamp, lastHash, cur_hash, data){
       this.timestamp=timestamp;
@@ -21,9 +23,13 @@ class block{
   static mineBlock(lastBlock,data){
     const timestamp = Date.now();
     const lastHash = lastBlock.cur_hash;
-    const cur_hash = 'yet to complete';
-    
+    const cur_hash = block.hash(timestamp,lastHash,data);
+
     return new this(timestamp,lastHash,cur_hash,data);
+  }
+
+  static hash(timestamp,lastHash,data){
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
   }
 }
 
