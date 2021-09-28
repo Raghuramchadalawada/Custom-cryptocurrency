03b06ef14576e8fbd84fa41ead66fecdeda8ef4c
Raghuramchadalawada <raghusrhyme@gmail.com>
2021-09-28 20:13:41 +0530

    Added dynamic difficulty adjustment

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,13 +1,14 @@
 const SHA256 = require('crypto-js/sha256');
-const {DIFFICULTY} = require('../config');
+const {DIFFICULTY, MINE_RATE} = require('../config');
 
 class block{
-  constructor(timestamp, lastHash, cur_hash, data, nonce){
+  constructor(timestamp, lastHash, cur_hash, data, nonce,difficulty){
       this.timestamp=timestamp;
       this.lastHash=lastHash;
       this.cur_hash=cur_hash;
       this.data=data;
       this.nonce = nonce;
+      this.difficulty = difficulty || DIFFICULTY;
   }
 //toString is mainly used for debugging
   toString(){
@@ -16,34 +17,44 @@ class block{
       Last hash    : ${this.lastHash.substring(0,10)}
       current hash : ${this.cur_hash.substring(0,10)}
       Nonce        : ${this.nonce}
+      Difficulty   : ${this.difficulty}
       data         : ${this.data}`;
   }
 //default nonce value is zero.(Last parameter in Genesis Block)
   static genesis(){
-    return new this('Dummy time','Nolast','firsthash',[],0);
+    return new this('Dummy time','Nolast','firsthash',[],0,DIFFICULTY);
   }
 
   static mineBlock(lastBlock,data){
     let cur_hash, timestamp;
     const lastHash = lastBlock.cur_hash;
+    let {difficulty} = lastBlock;
     let nonce = 0;
     do{ 
       nonce++;
       timestamp = Date.now()
-      cur_hash = block.hash(timestamp,lastHash,data,nonce);
-    }while (cur_hash.substring(0,DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+      difficulty = block.adjustDifficulty(lastBlock),timestamp;
+      cur_hash = block.hash(timestamp,lastHash,data,nonce,difficulty);
+    }while (cur_hash.substring(0,difficulty) !== '0'.repeat(difficulty));
     
 
-    return new this(timestamp,lastHash,cur_hash,data,nonce);
+    return new this(timestamp,lastHash,cur_hash,data,nonce,difficulty);
   }
 
-  static hash(timestamp,lastHash,data,nonce){
-    return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
+  static hash(timestamp,lastHash,data,nonce,difficulty){
+    return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
   }
 
   static blockHash(block) {
-    const { timestamp, lastHash, data, nonce} = block;
-    return this.hash(timestamp, lastHash, data, nonce);
+    const { timestamp, lastHash, data, nonce,difficulty} = block;
+    return this.hash(timestamp, lastHash, data, nonce,difficulty);
+  }
+
+  static adjustDifficulty(lastBlock,cur_time){
+    let {difficulty} = lastBlock; 
+    difficulty = lastBlock.timestamp + MINE_RATE > cur_time ?difficulty+1 : difficulty-1;
+    return difficulty;
+
   }
 }
 
--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -16,7 +16,10 @@ describe('Block',()=>{
         expect(block.lastHash).toEqual(lastBlock.cur_hash);
     });
     it('generates the hash that match the difficulty',()=>{
-        expect(block.cur_hash.substring(0,DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+        expect(block.cur_hash.substring(0,block.difficulty)).toEqual('0'.repeat(block.difficulty));
         console.log(block.toString());
+    });
+    it('Lowers the difficulty for slowly mined blocks',()=>{
+        expect(Block.adjustDifficulty(block,block.timestamp+360000)).toEqual(block.difficulty-1);
     });
 });
